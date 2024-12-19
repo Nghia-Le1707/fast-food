c560a7edecccf493ba0353c05431a8e6ebae1dd4
Nghia-Le1707 <nghiahuule101@gmail.com>
2024-12-19 13:24:44 +0700

    fastfood

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$('.list-food-s1').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplaySpeed: 2000,
+    autoplay: true,
+    dots: false,
+    arrows: false,
+  });
+  $('.list-drink-s2').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplaySpeed: 2000,
+    autoplay: true,
+    dots: false,
+    arrows: false,
+  });
+  $('.slide-feel-guest').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplaySpeed: 2000,
+    autoplay: true,
+    dots: false,
+    arrows: false,
+  });
+  $('.banner-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplaySpeed: 2000,
+    autoplay: true,
+    dots: true,
+    arrows: false,
+  });
